2b46263ea475c9c784d7233226fb41d6192152bf
Mark Hellner <mhellnerdev@gmail.com>
2022-01-02 01:11:07 -0500

    add basic cv2 resize functionality

--- script.py
+++ script.py
@@ -1,1 +1,15 @@
 import cv2
+
+
+img = cv2.imread("carina_nebula.tif", 1)
+
+print(type(img))
+print(img)
+print(img.shape)
+print(img.ndim)
+
+resized_image = cv2.resize(img, (1920, 1080))
+
+cv2.imshow("carina cordova", resized_image)
+cv2.waitKey(2000)
+cv2.destroyAllWindows()
