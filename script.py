feb28a8f8fbf12407d8ccfc8ed2849402d41ce2c
Mark Hellner <mhellnerdev@gmail.com>
2022-01-02 01:32:03 -0500

    add imwrite output function. image is saving correctly.

--- script.py
+++ script.py
@@ -9,10 +9,16 @@ img = cv2.imread("carina_nebula.tif", 1)
 # print(img.shape)
 # print(img.ndim)
 
+
+
+
+
 # assign new img variable with resize function applied
-resized_image = cv2.resize(img, (int(img.shape[1] / 2), int(img.shape[0] / 2)))
+half_size_img = cv2.resize(img, (int(img.shape[1] / 2), int(img.shape[0] / 2)))
+
 
 # create gui window to display image for x amount of seconds then destroy
-cv2.imshow("carina cordova", resized_image)
+cv2.imshow("carina cordova", half_size_img)
+cv2.imwrite("carina_resized.tif", half_size_img)
 cv2.waitKey(2000)
 cv2.destroyAllWindows()
