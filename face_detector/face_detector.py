8de4f43a2d99794424083fbc85c673d1b60c2633
Mark Hellner <mhellnerdev@gmail.com>
2022-01-02 22:37:51 -0500

    comments added

--- face_detector/face_detector.py
+++ face_detector/face_detector.py
@@ -1,26 +1,29 @@
 import cv2
 
+# call CascadeClassifier Function on xml model
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 # store image
-img = cv2.imread("photo.jpg")
+img = cv2.imread("news.jpg")
 
 # store grayscale version of image
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-
-faces = face_cascade.detectMultiScale(gray_img, 
-scaleFactor=1.05,
-minNeighbors=5)
+# create object variable of faces that have been detected with passed in function
+faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5)
 
 for x, y, w, h, in faces:
   img = cv2.rectangle(img, (x,y), (x + w, y + h), (0, 255, 0), 3)
 
+# debug info. prints cordinates of faces into arrays. numpy array.
 print(type(faces))
 print(faces)
 
-resized = cv2.resize(img, (int(img.shape[1] / 3), int(img.shape[0] / 3)))
+# resize image output by half original size
+resized = cv2.resize(img, (int(img.shape[1] / 2), int(img.shape[0] / 2)))
 
+
+# show window with rezised image passed in
 cv2.imshow("Gray Image", resized)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
